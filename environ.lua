57b05dc67220476d0d331cb1b477c6d249fc542e
Shane Allcroft <shaneallcroft@gmail.com>
2020-01-26 01:50:20 -0600

    machine learning files

---
+++ environ.lua
@@ -0,0 +1,100 @@
+local environ = {}
+local math = require "math"
+-- States are a tuple of dealer's first card (1-10) and the player's sum
+-- Set of actions
+
+environ.A = {'no input', 'P1 A', 'P1 B', 'P1 R', 'P1 Z', 'up full', 'up half', 'up right full', 'up right half', 'right full', 'right half', 'down right full', 'down right half', 'down full', 'down half', 'down left full', 'down left half', 'left full', 'left half', 'up left full', 'up left half', 'A + up full', 'A + up half', 'A + right full', 'A + right half', 'A + down full', 'A + down half', 'A + left full', 'A + left half', 'B + up full', 'B + down full', 'Z + left full', 'Z + right full'} --rolling
+-- 33 possible actions
+-- State transitions are not explicitly stored
+
+-- Draws a card (with replacement)
+local drawCard = function()
+  -- Draw number uniformly from 1-10
+  local num = torch.random(1, 10)
+  -- Draw colour red with p = 1/3 and black with p = 2/3
+  if torch.uniform() <= 1/3 then
+    return {num, 'red'}
+  else
+    return {num, 'black'}
+  end
+end
+
+-- Adds a card to an existing sum
+local addCard = function(sum, card)
+  if card[2] == 'black' then
+    return sum + card[1]
+  else
+    return sum - card[1]
+  end
+end
+
+-- Checks if bust
+local checkBust = function(sum)
+  return sum > 21 or sum < 1
+end
+
+-- Perform a step in the environment
+environ.step = function(s,a, newScore, oldScore)
+  -- Current state
+
+  local sPrime = {}
+
+  -- Enemy Death * (11000) + Self Death * (-10000) + Enemy Percent * (300) + Self Percent * (-143) + Self Destruct * (-30000) + 
+  local r = newScore - oldScore
+
+  return sPrime, r
+end
+
+
+--[[
+-- Performs a step in the environment
+environ.step = function(s, a)
+  -- Current state
+  local dealersFirstCard = s[1]
+  local playersSum = s[2]
+  -- Next state (initialised with deep copy of current state)
+  local sPrime = {}
+  sPrime[1] = dealersFirstCard
+  sPrime[2] = playersSum
+  -- Reward (0 by default)
+  local r = 0
+  -- Process actions
+  if a == 'hit' then
+    -- Draw and add next card
+    sPrime[2] = addCard(playersSum, drawCard())
+    -- Check player fail conditions
+    if checkBust(sPrime[2]) then
+      r = -1
+    end
+  elseif a == 'stick' then
+    local dealersSum = dealersFirstCard
+    -- Dealer sticks when on 17 or higher
+    while dealersSum < 17 and dealersSum >= 1 do
+      -- Draw and add next card
+      dealersSum = addCard(dealersSum, drawCard())
+    end
+    -- Check dealer fail conditions and winning conditions otherwise (player with largest sum)
+    if checkBust(dealersSum) or dealersSum < playersSum then
+      r = 1
+    elseif dealersSum > playersSum then
+      r = -1
+    end
+  end
+  return sPrime, r
+end
+]]--
+
+-- Calculates if the current state is terminal given previous action and reward
+environ.isTerminal = function(a, r)
+  return a == 'stick' or r == -1
+end
+
+environ.cartesianDistanceFromEnemy = function()
+    Xdiff = self_x - enemy_x
+    Ydiff = self_y - enemy_y
+    XdiffSquared = Xdiff * Xdiff
+    YdiffSquared = Ydiff * Ydiff
+    return math.sqrt(XdiffSquared + YdiffSquared)
+end
+
+return environ
---
+++ policy_gradient.lua
@@ -0,0 +1,164 @@
+local _ = require 'moses'
+local nn = require 'nn'
+local gnuplot = require 'gnuplot'
+local environ = require 'environ'
+local smash64 = require 'smash64'
+
+-- Set manual seed
+torch.manualSeed(1)
+
+-- Load Q* from MC control
+local QStar = torch.load('Q.t7')
+-- Extract V as argmax Q
+local V = torch.max(QStar, 3):squeeze()
+
+local nEpisodes = 100
+-- Number of discrete actions
+local m = #environ.A
+
+-- Initial exploration ɛ
+local epsilon = 1
+-- Linear ɛ decay
+local epsilonDecay = 1/nEpisodes
+-- Minimum ɛ
+local epsilonMin = 0.05
+-- Constant step-size ɑ
+local alpha = 0.001
+-- RMSProp decay
+local decay = 0.9
+-- (Machine) epsilon
+local eps = 1e-20
+-- Entropy regularisation factor β
+local beta = 0.01
+
+PATH = "smashModel.pt"
+local net
+if os.isfile(PATH) then
+  net = torch.load(PATH)
+else
+
+  -- Create policy network π
+  net = nn.Sequential()
+  input = 15
+  net:add(nn.Linear(input, 16))
+  net:add(nn.ReLU(true))
+  net:add(nn.Linear(16, m))
+  net:add(nn.SoftMax())
+end
+
+-- Get network parameters θ
+local theta, gradTheta = net:getParameters()
+-- Moving average of squared gradient
+local gradThetaSq = torch.Tensor(gradTheta:size()):zero()
+
+-- Results from each episode
+local results = torch.Tensor(nEpisodes)
+
+-- Sample
+for i = 1, nEpisodes do
+  --TODO: Start new game and load the new game's first state accordingly 
+
+
+
+  -- Experience tuples (s, a, r)
+  local E = {}
+  -- {bot death state, bot damage taken, bot x pos, bot y, bot xvel, bot yvel, }
+  local s = { self_deaths, self_percent, self_x, self_y, self_xvel, self_yvel, 
+              enemy_deaths, enemy_percent, enemy_x, enemy_y, enemy_xvel, enemy_yvel, enemy_name} 
+  
+  -- Run till termination
+  repeat
+    -- Choose action by ɛ-greedy exploration
+    local aIndex
+    if torch.uniform() < (1 - epsilon) then -- Exploit with probability 1 - ɛ
+      -- Get categorical action distribution from π = p(s; θ)
+      local probs = net:forward(torch.Tensor(s))
+      probs:add(eps) -- Add small probability to prevent NaNs
+      -- Sample action ~ p(s; θ)
+      aIndex = torch.multinomial(probs, 1)[1]
+    else
+      -- Otherwise pick any action with probability 1/m
+      aIndex = torch.random(1, m)
+    end
+    local a = environ.A[aIndex]
+
+    local oldS = s
+    local oldScore = smash64.score_points
+    -- Perform a step
+
+    -- Have player perform Action
+
+    -- Set new s based on new Action
+    s = { self_deaths, self_percent, self_x, self_y, self_xvel, self_yvel, 
+              enemy_deaths, enemy_percent, enemy_x, enemy_y, enemy_xvel, enemy_yvel, enemy_name} 
+
+
+    -- Score based on how well the action performed
+    local sPrime, r = environ.step(s, a, oldScore) -- r comes from score function f(s)
+
+    -- Store experience tuple
+    table.insert(E, {oldS, a, r})
+
+    -- Linearly decay ɛ
+    epsilon = math.max(epsilon - epsilonDecay, epsilonMin)
+  until environ.isTerminal(a, r)
+  
+  -- Save result of episode
+  results[i] = E[#E][3]
+  
+  -- Reset ∇θ
+  gradTheta:zero()
+  
+  -- Learn from experience of one complete episode
+  for j = 1, #E do
+    -- Extract experience
+    local s = E[j][1]
+    local a = E[j][2]
+    -- Get action index
+    local aIndex = _.find(environ.A, a)
+
+    -- Calculate variance-reduced reward (advantage) ∑t r - b(s) = ∑t r - V(s) = A
+    local A = 0
+    for k = j, #E do
+      A = A + (E[k][3] - V[s[1]][s[2]])
+    end
+
+    -- Use a policy gradient update (REINFORCE rule): ∇θ Es[f(s)] = ∇θ ∑s p(s)f(s) = Es[f(s) ∇θ logp(s)]
+    local input = torch.Tensor(s)
+    local output = net:forward(input)
+    output:add(eps) -- Add small probability to prevent NaNs
+
+    -- ∇θ logp(s) = 1/p(a) for chosen a, 0 otherwise
+    local target = torch.zeros(m)
+    target[aIndex] = A * 1/output[aIndex] -- f(s) ∇θ logp(s)
+
+    -- Calculate gradient of entropy of policy: -logp(s) - 1
+    local gradEntropy = -torch.log(output) - 1
+    -- Add to target to improve exploration (prevent convergence to suboptimal deterministic policy)
+    target:add(beta * gradEntropy)
+    
+    -- Accumulate gradients
+    net:backward(input, target)
+  end
+
+  -- Update moving average of squared gradients
+  gradThetaSq = decay * gradThetaSq + (1 - decay) * torch.pow(gradTheta, 2)
+  -- RMSProp update (gradient ascent version)
+  theta:add(torch.cdiv(alpha * gradTheta, torch.sqrt(gradThetaSq) + eps))
+end
+
+-- Take average results over 1000 episodes
+local avgResults = torch.Tensor(nEpisodes/1000)
+for ep = 1, nEpisodes, 1000 do
+  avgResults[(ep - 1)/1000 + 1] = torch.mean(results:narrow(1, ep, 1000))
+end
+
+-- Plot results
+gnuplot.pngfigure('PolicyGradient.png')
+gnuplot.plot('Average Result', torch.linspace(1, nEpisodes/1000, nEpisodes/1000), avgResults)
+gnuplot.title('Policy Gradient Results')
+gnuplot.ylabel('Result (Mean over 1000 Episodes)')
+gnuplot.xlabel('Episode (x1000)')
+gnuplot.plotflush()
+
+torch.save(net, PATH)
